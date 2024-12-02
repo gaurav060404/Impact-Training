5015f6d5f6c58480deb56f1c00c1fc7fe325b02a
gaurav060404 <gs762282@gmail.com>
2024-12-02 15:41:58 +0530

    Update content in index.html, enhance event handling in script.js, and improve layout styles in style.css

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ for (let index = 0; index < items.length; index++) {
     const item = items[index];
     item.addEventListener('click',(e)=>{
         e.preventDefault();
+        e.stopPropagation();
         console.log(`${item.innerHTML} clicked!!!`);
         
     })
