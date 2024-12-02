ad6bca2d05fe817dc2e3a2b70ae874a97b38cbcc
gaurav060404 <gs762282@gmail.com>
2024-12-02 16:21:40 +0530

    Refactor index.html structure, comment out unused script.js code, and enhance style.css with improved padding and hover effects

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const items = document.querySelectorAll(".items");
-for (let index = 0; index < items.length; index++) {
-    const item = items[index];
-    item.addEventListener('click',(e)=>{
-        e.preventDefault();
-        e.stopPropagation();
-        console.log(`${item.innerHTML} clicked!!!`);
+// const items = document.querySelectorAll(".items");
+// for (let index = 0; index < items.length; index++) {
+//     const item = items[index];
+//     item.addEventListener('click',(e)=>{
+//         e.preventDefault();
+//         e.stopPropagation();
+//         console.log(`${item.innerHTML} clicked!!!`);
         
-    })
-}
+//     })
+// }
